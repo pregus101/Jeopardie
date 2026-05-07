853943693c2bbbf9c7846af1ffbd63763ed74622
pregus101 <159173502+pregus101@users.noreply.github.com>
2026-05-06 23:55:06 -0700

    Updated questions and answers

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const clue = (text, question, answer, extra = {}) => ({
   text,
   color: "#11f",
   textColor: "white",
-  textSize: "20px",
+  textSize: "20px", 
   question,
   answer,
   ...extra,
@@ -40,39 +40,39 @@ const categories = [
 
 const clueRows = [
   [
-    clue("$100", "What is solar energy?", "Energy from sunlight."), //column 1
-    clue("$100", "What is wind power?", "Energy from moving air."), //column 2
-    clue("$100", "2 + 2?", "4"), //column 3
-    clue("$100", "What color is the sky?", "Usually blue."), //column 4
-    clue("$100", "What planet do we live on?", "Earth."), //column 5
+    clue("$100", "\“Renewables are always clean and non-renewables always release greenhouse gases.\”Two examples (one for each) that disprove this statement.", "Nuclear fission energy and biomass."), //column 1
+    clue("$100", "What are the two type of solar energies?", "What is passive and active solar (will accept Concentrated Solar and Heat Pump)"), //column 2
+    clue("$100", "The source of energy that less developed countries typically use abundantly.", "What is biomass?"), //column 3
+    clue("$100", "If each house consumes 10kwh of electricity per day and the nearby solar farm produces 200kwh per day how many houses can the solar farm power?", "What is 20 houses?"), //column 4
+    clue("$100", "A Sustainable Development Goal pertaining to clean energy.", "(any one) What is 7: Affordable and clean energy, 9: industry, innovation, and infrastructure, 11: Sustainable cities and communities, 12: Responsible consumption and production?."), //column 5
   ],
   [
-    clue("$200", "Edit question 1", "Edit answer 1"), //column 1
-    clue("$200", "Edit question 2", "Edit answer 2"), //column 2
-    clue("$200", "Edit question 3", "Edit answer 3"), //column 3
-    clue("$200", "Edit question 4", "Edit answer 4"), //column 4
-    clue("$200", "Edit question 5", "Edit answer 5"), //column 5
+    clue("$200", "Name 5 renewable energy sources.", "(in any order) What are biomass, geothermal, wind, solar, and hydroelectric?"), //column 1
+    clue("$200", "A category that wind and solar energy fall in, but biomass doesn’t.", "What is nondepletable energy?"), //column 2
+    clue("$200", "A clean energy source that can act like a battery and about 80% efficient in generation of electricity", "What is a hydrogen fuel cell?"), //column 3
+    clue("$200", "A family of four used a 500 watt microwave for 4 hours a day for 30 days out of a month. They switched to a 300 watt microwave. How many kilowatt hours of energy did they save?", "What is 24 kWh?"), //column 4
+    clue("$200", "A reason why the renewable energy sucks for each energy.", "(one from each)Wind: What is taking up space, what is killing birds, what is being loud?Hydroelectric: What is harming fish migration?Solar: What is hazardous waste produced as a by product during manufacturing?"), //column 5
   ],
   [
-    clue("$300", "Edit question 6", "Edit answer 6"), //column 1
-    clue("$300", "Edit question 7", "Edit answer 7"), //column 2
-    clue("$300", "Edit question 8", "Edit answer 8"), //column 3
-    clue("$300", "Edit question 9", "Edit answer 9"), //column 4
-    clue("$300", "Edit question 10", "Edit answer 10"), //column 5
+    clue("$300", "The percent of total energy that comes from renewables.", "What is ~20%?"), //column 1
+    clue("$300", "The renewable energy that is the most used around the world overall.", "What is Hydroelectric Power?"), //column 2
+    clue("$300", "The root source of geothermal energy.", "What is radioactive decay in Earth’s core?"), //column 3
+    clue("$300", "How much energy will the family of four save by switching from a 60-watt incandescent bulb for their kitchen vent light to a 9-watt LED if they run the light for 10 hours a day for 30 days?", "What is 15.3 kWh?"), //column 4
+    clue("$300", "Our FAVORITE method of converting ANYTHING into electricity.", "WHAT IS A STEAM TURBINE!?"), //column 5
   ],
   [
-    clue("$400", "Edit question 11", "Edit answer 11"), //column 1
-    clue("$400", "Edit question 12", "Edit answer 12"), //column 2
-    clue("$400", "Edit question 13", "Edit answer 13"), //column 3
-    clue("$400", "Edit question 14", "Edit answer 14"), //column 4
-    clue("$400", "Edit question 15", "Edit answer 15"), //column 5
+    clue("$400", "The country that consumes the most percent of their energy as renewable energy in 2024.\n \n(x2) Within one percent, the percentage of their energy consumed was from renewable sources.", "What is Iceland? \n \n What is 80.5%?"), //column 1
+    clue("$400", "A problem that wind, solar, and hydroelectric all share around energy input.(x2) A solution to this problem", "What are fluctuating input levels?What is a smart grid?"), //column 2
+    clue("$400", "Two ways to use biomass.(x2) Two MORE ways to use biomass", "What is direct combustion (fire), pyrolysis (charcoal), anaerobic digestion (methane), and aerobic fermentation (ethanol)?+ Point per correct- Point per incorrect2 pts = 4004pts = 800No half points!"), //column 3
+    clue("$400", "A city burns coal for most of their energy. They spend $5000 per month on coal for said energy. The city wants to instead install solar panels, which will cost $400,000 to install and $500 per year to maintain. It will take _______ years to break even (make back the costs).", "What is 7? (>6.722)"), //column 4
+    clue("$400", "What materials are typically used for biomass?", "What is Wood, Animal waste, Algae, and Agricultural leftovers"), //column 5
   ],
   [
-    clue("$500", "Edit question 16", "Edit answer 16"), //column 1
-    clue("$500", "Edit question 17", "Edit answer 17"), //column 2
-    clue("$500", "Edit question 18", "Edit answer 18"), //column 3
-    clue("$500", "Edit question 19", "Edit answer 19"), //column 4
-    clue("$500", "Edit question 20", "Edit answer 20"), //column 5
+    clue("$500", "The three pillars of sustainable development.", "What are economy, society, and environment?"), //column 1
+    clue("$500", "The renewable energy that comes from the MOON!", "What is Tidal Energy?"), //column 2
+    clue("$500", "Percentage of corn used to make ethanol.", "What is 40%?"), //column 3
+    clue("$500", "Every house in a neighborhood consumes 800 watt-hours of energy each time they use the washing machine. A new Energy Star washing machine only consumes 450 watt-hours each use. If 80% of the neighborhood of 50 houses switched to Energy Star washing machines, they would save about _________ of energy in a year, given they use the machine every 5 days.", "What is 1022 kWh?"), //column 4
+    clue("$500", "List 7 clean energies", "What is solar, wind, geothermal, nuclear fusion, nuclear fission, hydrogen, hydro, osmotic power, oceanic thermal, Piezoelectricity, Triboelectric Nanogenerators, Microbial Fuel cells, and small Modular reactors?"), //column 5
   ],
 ];
 
@@ -82,8 +82,8 @@ const clueRows = [
 
 //appears after you finish all other questions (click question and then click esc to skip it)
 const finalJeopardy = {
-  question: "Write your Final Jeopardy question here.",
-  answer: "Write your Final Jeopardy answer here.",
+  question: "I am the of the classification that shan't prioritize the finite but instead the abundant. I am able to emit or not to emit I may include that of dams or possibly that of the burning of waste, but I shan’t burn that made of oil or coal. What am I?",
+  answer: "What is Renewable energy?",
 };
 
 const grid = document.getElementById("grid");
